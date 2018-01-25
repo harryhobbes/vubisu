59f41a87e4a588ca3f70b3208e49692bd924cfdc
harryhobbes <harryhobbes@gmail.com>
2018-01-25 19:58:30 +1000

    - Service onClick working

--- app/src/main/java/com/vubisu/acs/ServiceViewHolder.java
+++ app/src/main/java/com/vubisu/acs/ServiceViewHolder.java
@@ -1,7 +1,10 @@
 package com.vubisu.acs;
 
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -11,13 +14,14 @@ import com.vubisu.acs.databinding.ItemServiceBinding;
  * Created by harryhobbes on 20/1/18.
  */
 
-public class ServiceViewHolder extends RecyclerView.ViewHolder{
+public class ServiceViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     ItemServiceBinding itemServiceBinding;
     int recordId;
 
     public ServiceViewHolder(View itemView) {
         super(itemView);
         itemServiceBinding = DataBindingUtil.bind(itemView);
+        itemView.setOnClickListener(this);
     }
 
     public void bindCursor(Cursor cursor) {
@@ -26,5 +30,17 @@ public class ServiceViewHolder extends RecyclerView.ViewHolder{
         itemServiceBinding.txtName.setText(cursor.getString(cursor.getColumnIndexOrThrow(Service.KEY_NAME)));
         itemServiceBinding.txtGroup.setText(cursor.getString(cursor.getColumnIndexOrThrow(Service.KEY_GROUP)));
         itemServiceBinding.txtPrice.setText(cursor.getString(cursor.getColumnIndexOrThrow(Service.KEY_PRICE)));
+    }
+
+    @Override
+    public void onClick(View view) {
+        Context mContext = view.getContext();
+        Intent mIntent = new Intent(mContext, com.vubisu.acs.ServiceViewActivity.class);
+
+        Bundle mBundle = new Bundle();
+        mBundle.putInt("recordId", recordId);
+
+        mIntent.putExtras(mBundle);
+        mContext.startActivity(mIntent);
     }
 }
